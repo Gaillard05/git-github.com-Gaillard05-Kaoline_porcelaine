76c7a59fdae4111c154d5e3bac8d17594f9493d4
Maylis Gaillard <maylisgaillard@hotmail.fr>
2021-11-24 22:21:11 +0100

    ajouter page contact, a propos, login, créer un compte

---
+++ migrations/Version20211122131737.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211122131737 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pictures DROP FOREIGN KEY FK_8F7C2FC06C8A81A9');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE pictures');
+        $this->addSql('DROP TABLE products');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pictures (id INT AUTO_INCREMENT NOT NULL, products_id INT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_8F7C2FC06C8A81A9 (products_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prices DOUBLE PRECISION NOT NULL, categories VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE pictures ADD CONSTRAINT FK_8F7C2FC06C8A81A9 FOREIGN KEY (products_id) REFERENCES products (id)');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
---
+++ src/Controller/PdoController.php
@@ -0,0 +1,87 @@
+<?php
+// include("int.php");
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'kaoline_porcelaine';
+
+
+try {
+    //     //On établit la connexion
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", 'root', '');
+
+    // On définit le mode d'erreur de PDO sur Exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo 'Connexion réussie';
+}
+
+/*On capture les exceptions si une exception est lancée et on affiche
+                     *les informations relatives à celle-ci*/ catch (PDOException $e) {
+    echo ("Erreur : " . $e->getMessage());
+}
+// print_r($_POST);
+
+$Nom = $_POST["Nom"];
+$Prenom = $_POST["Prenom"];
+$Email = $_POST["Email"];
+$Telephone = $_POST["Telephone"];
+$Message = $_POST["Message"];
+
+
+echo 'Nom : ' . $_POST["Nom"] . '<br>';
+echo 'Prenom : ' . $_POST["Prenom"] . '<br>';
+echo 'Email : ' . $_POST["Email"] . '<br>';
+echo 'Telephone : ' . $_POST["Telephone"] . '<br>';
+echo 'Message : ' . $_POST["Message"] . '<br>';
+
+
+// $Nom = valid_donnees($_POST["Nom"]);
+// $Prenom = valid_donnees($_POST["Prenom"]);
+// $Email = valid_donnees($_POST["Email"]);
+// $Telephone = valid_donnees($_POST["Telephone"]);
+// $Message = valid_donnees($_POST["Message"]);
+
+// function valid_donnees($donnees)
+// {
+//     $donnees = trim($donnees);
+//     $donnees = stripslashes($donnees);
+//     $donnees = htmlspecialchars($donnees);
+//     return $donnees;
+// }
+
+// /*Si les champs prenom et mail ne sont pas vides et si les donnees ont
+//        *bien la forme attendue...*/
+// // if (
+// !empty($Prenom)
+//     && strlen($Prenom) <= 20
+//     && preg_match("/^[A-Za-z '-]+$/", $Prenom)
+//     && !empty($Email)
+//     && filter_var($Email, FILTER_VALIDATE_EMAIL);
+
+try {
+
+    //On se connecte à la BDD
+    $dbco = new PDO("mysql:host=$servername;dbname=$dbname", "root", "");
+    $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //On insère les données reçues
+
+    $sth = $dbco->prepare("INSERT INTO Contact02 (Nom,  Prenom, Email, Telephone, Message)
+                  VALUES(:Nom, :Prenom, :Email, :Telephone, :Message)");
+    $sth->bindParam(':Nom', $Nom);
+    $sth->bindParam(':Prenom', $Prenom);
+    $sth->bindParam(':Email', $Email);
+    $sth->bindParam(':Telephone', $Telephone);
+    $sth->bindParam(':Message', $Message);
+
+    //   echo ""
+
+    $sth->execute();
+    //On renvoie l'utilisateur vers la page de remerciement
+    // header("Location: Contact.html.twig");
+} catch (PDOException $e) {
+    echo 'Erreur : ' . $e->getMessage();
+}
+    // } 
+    // else {
+    //     header("Location:index2.php");
+//   }
