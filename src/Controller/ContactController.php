2def72a1f2b3c46d7b6bb5634acb1d15f63bb854
Maylis Gaillard <maylisgaillard@hotmail.fr>
2022-02-18 19:21:12 +0100

    modification adresse mail envoi

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -49,7 +49,7 @@ class ContactController extends AbstractController
 
             $mail = (new TemplatedEmail())
                 ->from($contact->getEmail())
-                ->to('client.kaolineporcelaine@gmail.com')
+                ->to('sv.client.kaolineporcelaine@gmail.com')
                 ->subject('Time for Symfony Mailer!')
                 ->htmlTemplate('emails/demande.html.twig')
                 ->context([
